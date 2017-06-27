c0a52a4e3096bed5551834489eee30c1a7cbf677
z-mkat <z.meah@yahoo.com>
2017-06-27 21:05:25 +0100

    Initial check-in of module GymApp.iOS

---
+++ AppDelegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace GymApp.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+
+            LoadApplication(new App());
+
+            UINavigationBar.Appearance.BarTintColor = UIColor.FromRGB(0, 56, 99);
+            UINavigationBar.Appearance.BarStyle = UIBarStyle.Black;
+            UINavigationBar.Appearance.TintColor = UIColor.White;
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
---
+++ Persistance/SQLiteDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GymApp.iOS;
+using SQLite;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+
+namespace GymApp.iOS
+{
+    public class SQLiteDb : ISQLiteDb
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = System.IO.Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
